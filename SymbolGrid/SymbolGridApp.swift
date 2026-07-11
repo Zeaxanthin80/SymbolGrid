2d553dccfb7144815b35932a948b5fb0e18c17b2
Jose Marquez <jose.marquez012@mymdc.net>
2026-07-11 13:43:55 -0400

    Initial Commit

---
+++ SymbolGrid/SymbolGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymbolGridApp.swift
+//  SymbolGrid
+//
+//  Created by user on 7/11/26.
+//
+
+import SwiftUI
+
+@main
+struct SymbolGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
